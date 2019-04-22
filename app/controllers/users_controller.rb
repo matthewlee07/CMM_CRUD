ea772da78accd36950f22d94612dea38589b40ab
Matt Lee <matthewlee07@gmail.com>
2019-04-22 02:24:20 -0400

    friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   # CREATE
   def create 
     @user = User.new(user_params)
@@ -23,11 +25,37 @@ class UsersController < ApplicationController
   end
 
   #  UPDATE
+  def edit 
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Updated user"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   # DESTROY
 
   private 
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
     end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,26 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user1 = users(:one)
+    @user2 = users(:two)
+  end
+
   test "should get new" do
     get users_new_path
     assert_response :success
   end
 
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user1)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user1), params: { user1: { username: @user1.username, email: @user1.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
 end
