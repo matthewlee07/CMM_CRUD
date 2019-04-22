30081914698f20486eff6e08fafaeebb33275c27
Matt Lee <matthewlee07@gmail.com>
2019-04-21 20:35:34 -0400

    user signup integration testing

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid user signup information" do
+    get users_new_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { username:  ""} }
+    end
+    assert_template 'users/new'
+  end
+
+  test "valid user signup information" do
+    get users_new_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { 
+        username:  "Example User", 
+        email: "user@example.com", 
+        password: "password"} }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    
+  end
+end
