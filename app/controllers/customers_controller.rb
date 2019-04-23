ebd21b049d87994ff387584bc6046f789486ce22
Matt Lee <matthewlee07@gmail.com>
2019-04-22 22:30:55 -0400

    created controller

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,22 @@
+class CustomersController < ApplicationController
+  def create
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
